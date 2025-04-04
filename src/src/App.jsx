b433e0997a1eff310ac74a1ff13ef2271d909e39
Le Ngoc Hai <leensea96@gmail.com>
2025-04-04 13:26:14 +0700

    update src: update deployment version to ga-0.0.4

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import Heading from "./components/Heading.jsx";
+import Items from "./components/Items.jsx";
+import Subscribe from "./components/Subscribe.jsx";
+import SignIn from "./components/SignIn.jsx";
+import "./index.css";
 function App() {
-  const [count, setCount] = useState(0)
+  //state to assure only authenticated user can see content
+  const [visible, setVisible] = useState(false);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SignIn
+        visible={visible}
+        onSignIn={() => {
+          setVisible(true);
+        }}
+        onSignOut={() => {
+          setVisible(false);
+        }}
+      />
+      <Heading />
+      <Subscribe 
+        onSignIn={() => {
+          setVisible(true);
+        }}/>
+      <Items visible={visible} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
